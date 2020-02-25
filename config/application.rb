eb8720dc7f50b3c726a9833e47e9b0c7fdb1c8e4
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-02-24 22:09:07 -0300

    Setting email configuration

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'dotenv'
+Dotenv.load('keys.env')
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +14,18 @@ module ElephantRH
     config.load_defaults 5.2
 
     config.i18n.default_locale = "pt-BR"
+
+    config.action_mailer.default_url_options = { host: "localhost:3000" }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 25,
+      domain:               'gmail.com',
+      user_name:            "#{ENV["EMAIL_KEY"]}",
+      password:             "#{ENV["PASSWORD_KEY"]}",
+      authentication:       :plain,
+      enable_starttls_auto: true
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,3 @@
-require 'dotenv'
-Dotenv.load('keys.env')
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -81,19 +78,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address:              ENV["SMTP_ADDRESS"],
-    port:                 ENV["SMTP_PORT"],
-    domain:               ENV["SMTP_DOMAIN"],
-    user_name:            ENV["SMTP_USER"],
-    password:             ENV["SMTP_PASSWORD"],
-    authentication:       :plain,
-    enable_starttls_auto: true
-  }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
